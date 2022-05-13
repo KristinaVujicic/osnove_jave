d6fd3aae0af29c5fd65597fffbbcf0286f2c3389
Kristina Vujicic <xtinakg.93@gmail.com>
2022-05-13 19:59:18 +0200

    Minor typo fix

--- osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
+++ osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
@@ -2,7 +2,9 @@ package p13_05_2022;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello borld!");
+        System.out.println("Hello World!");
+        System.out.println("Novi print");
+
     }
 
 }
