a178d4bf9d807d6782eaf0cc7960e383e184cf74
Kristina Vujicic <xtinakg.93@gmail.com>
2022-05-13 20:34:20 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,8 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("Git naredbe: clone, status, add, commit-m, push.");
+        System.out.print("Takodje imamo opciju clear, touch i delete.");
+    }
+}
