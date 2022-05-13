410790ab0f362de75428f455b2872a21d6b810b8
Kristina Vujicic <xtinakg.93@gmail.com>
2022-05-13 21:47:43 +0200

    Delete class

--- osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1_updated.java
+++
@@ -1,8 +0,0 @@
-package p13_05_2022;
-
-public class Git_Zadatak1_updated {
-    public static void main(String[] args) {
-        System.out.println("Git naredbe: clone, status, add, commit-m, push.");
-        System.out.print("Takodje imamo opciju clear, touch i delete.");
-    }
-}
